c65afd9b001c6b9a1f5031a028faf67019359d3c
Kevin Campos <4038983+kevinmcampos@users.noreply.github.com>
2018-06-03 23:05:51 +0200

    Initial commit, the bad code with god objects

---
+++ app/src/main/java/me/kevincampos/catsdagger/FavoriteModel.java
@@ -0,0 +1,19 @@
+package me.kevincampos.catsdagger;
+
+public class FavoriteModel {
+    private long timeAdded;
+    private String url;
+
+    public FavoriteModel(long timeAdded, String url) {
+        this.timeAdded = timeAdded;
+        this.url = url;
+    }
+
+    public long getTimeAdded() {
+        return timeAdded;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
---
+++ app/src/main/java/me/kevincampos/catsdagger/cat_api/CatImageModel.java
@@ -0,0 +1,16 @@
+package me.kevincampos.catsdagger.cat_api;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+@Root(name = "image")
+public class CatImageModel {
+    @Element(name = "id")
+    public String id;
+
+    @Element(name = "url")
+    public String url;
+
+    @Element(name = "source_url")
+    public String sourceUrl;
+}
