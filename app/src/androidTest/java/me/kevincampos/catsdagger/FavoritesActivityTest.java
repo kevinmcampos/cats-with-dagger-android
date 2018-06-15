738297f21e82bd7922e0fe60f1acabedc61cb4d6
Kevin Campos <4038983+kevinmcampos@users.noreply.github.com>
2018-06-15 23:08:11 +0200

    Test FavoritesActivity in presentation layer onlu by mocking GetFavoritesUseCase

--- app/src/androidTest/java/me/kevincampos/catsdagger/FavoritesActivityTest.java
+++ app/src/androidTest/java/me/kevincampos/catsdagger/FavoritesActivityTest.java
@@ -1,9 +1,10 @@
 package me.kevincampos.catsdagger;
 
+import android.content.Intent;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,63 +12,50 @@ import org.junit.runner.RunWith;
 import java.util.ArrayList;
 import java.util.List;
 
-import me.kevincampos.catsdagger.di.AppDIComponent;
-import me.kevincampos.catsdagger.di.FavoriteRepoDIModule;
-import me.kevincampos.catsdagger.di.UserDIComponent;
-import me.kevincampos.catsdagger.favorites.FavoriteModel;
+import me.kevincampos.catsdagger.di.FavoritesActivityDIModule;
 import me.kevincampos.catsdagger.favorites.FavoriteRepository;
+import me.kevincampos.catsdagger.favorites.GetFavoritesUseCase;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.mockito.Mockito.mock;
 
 @RunWith(AndroidJUnit4.class)
 public class FavoritesActivityTest {
 
     @Rule
     public ActivityTestRule<FavoritesActivity> activityRule =
-            new ActivityTestRule<>(FavoritesActivity.class);
-
-    @BeforeClass
-    public static void beforeClass() {
-        UserDIComponent.initialize(new FavoriteRepoDIModule() {
-            @Override
-            public AppDIComponent getAppDIComponent() {
-                return AppDIComponent.get();
-            }
-
-            @Override
-            public FavoriteRepository provideFavoriteRepository() {
-                return new FavoriteRepository() {
-                    @Override
-                    public List<FavoriteModel> getFavorites() {
-                        ArrayList<FavoriteModel> models = new ArrayList<>();
-                        models.add(new FavoriteModel(10, "url-10"));
-                        models.add(new FavoriteModel(11, "url-11"));
-                        models.add(new FavoriteModel(12, "url-12"));
-                        return models;
-                    }
-
-                    @Override
-                    public List<FavoriteModel> addFavorite(FavoriteModel model) {
-                        throw new RuntimeException("Not implemented");
-                    }
-
-                    @Override
-                    public void registerChangeListener(FavoriteRepository.ChangeListener listener) {
-                        // NoOp
-                    }
-
-                    @Override
-                    public void clearChangeListener() {
-                        // NoOp
-                    }
-                };
-            }
-        });
-    }
+            new ActivityTestRule<>(FavoritesActivity.class, true, false);
 
     @Test
     public void testThereAreThreeImages() {
-        onView(withId(R.id.favorites_rv)).check(new RecyclerViewItemCountAssertion(3));
+        final ArrayList<String> expectedUrls = new ArrayList<>();
+        expectedUrls.add("url0");
+        expectedUrls.add("url1");
+        expectedUrls.add("url2");
+        setUpGetFavoritesResponse(expectedUrls);
+
+        activityRule.launchActivity(new Intent());
+
+        onView(ViewMatchers.withId(R.id.favorites_rv)).check(new RecyclerViewItemCountAssertion(3));
     }
+
+    @Test
+    public void testThereAreZeroImages() {
+        final ArrayList<String> emptyUrls = new ArrayList<>();
+        setUpGetFavoritesResponse(emptyUrls);
+
+        activityRule.launchActivity(new Intent());
+
+        onView(ViewMatchers.withId(R.id.favorites_rv)).check(new RecyclerViewItemCountAssertion(0));
+    }
+
+    private void setUpGetFavoritesResponse(final List<String> urls) {
+        FavoritesActivityDIModule.testGetFavoritesUseCase = new GetFavoritesUseCase(mock(FavoriteRepository.class)) {
+            @Override
+            public void getFavoritesUrl(Callback callback) {
+                callback.favoriteUrlsUpdated(urls);
+            }
+        };
+    }
+
 }
--- app/src/main/java/me/kevincampos/catsdagger/di/FavoritesActivityDIModule.java
+++ app/src/main/java/me/kevincampos/catsdagger/di/FavoritesActivityDIModule.java
@@ -6,11 +6,16 @@ public class FavoritesActivityDIModule {
 
     private UserDIComponent userDIComponent;
 
+    public static GetFavoritesUseCase testGetFavoritesUseCase;
+
     public FavoritesActivityDIModule(UserDIComponent userDIComponent) {
         this.userDIComponent = userDIComponent;
     }
 
     GetFavoritesUseCase provideGetFavoritesUseCase() {
+        if (testGetFavoritesUseCase != null) {
+            return testGetFavoritesUseCase;
+        }
         return new GetFavoritesUseCase(userDIComponent.getFavoriteRepository());
     }
 
